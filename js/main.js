7af9714ebae41be29a5c98ab6991c004b153968d
Gonzalo <gonzalo.d.rolando@gmail.com>
2023-07-03 18:41:00 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+function handleSubmit(evento) {
+
+event.preventDefault()
+var fll = document.getElementById("fecha-llegada").value
+var fsa = document.getElementById("fecha-salida").value
+
+var fechaLlegada = new Date(fll + " 00:00 -0300")
+var fechaSalida = new Date(fsa + " 00:00 -0300")
+
+if (fechaSalida < fechaLlegada) {
+  alert("La fecha de salida debe ser posterior a la fecha de llegada.")
+        }
+    else {
+        alert("Usted ingresará al hotel el día " + fechaLlegada.toLocaleDateString())
+        alert("Usted saldrá del hotel el día " + fechaSalida.toLocaleDateString())
+    }
+
+let valid = false
+
+//var hab = document.getElementById("habitacion")
+//alert("Usted eligió la habitación " + hab.options[hab.selectedIndex].text)
+
+do {
+    let habitacionIngresada = prompt("Ingresar Habitación")
+    if (!isNaN(parseInt(habitacionIngresada))) {
+        alert("El dato ingresado es erróneo. Ingrese un tipo de habitación")
+        }
+    else {
+        valid = true
+        alert("La habitación seleccionada es " + habitacionIngresada)
+        }
+    }while(!valid)
+}
+
+document.getElementById("reservas-form").addEventListener("submit",handleSubmit)
+
